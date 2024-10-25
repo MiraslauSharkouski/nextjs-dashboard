b2ad9c76d816f0654798ec97b0b6d06d7e421d64
Miraslau <miraslausharkouski@gmail.com>
2024-10-25 21:11:47 +0300

    add back button component

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,8 +1,10 @@
 import PostsTable from "@/components/posts/PostsTable";
+import BackButton from "@/components/BackButton";
 
 const PostPage = () => {
   return (
     <>
+      <BackButton text="Go Back" link="/" />
       <PostsTable />
     </>
   );
