9a04dfbe5f41442ec10609ca35ddbea38288e12a
Miraslau <miraslausharkouski@gmail.com>
2024-10-26 11:21:43 +0300

    get post data

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -17,7 +17,30 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import posts from "@/data/posts";
 
-const PostEditPage = () => {
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required",
+  }),
+  body: z.string().min(1, {
+    message: "Body is required",
+  }),
+  author: z.string().min(1, {
+    message: "Author is required",
+  }),
+  date: z.string().min(1, {
+    message: "Date is required",
+  }),
+});
+
+interface PostEditPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const PostEditPage = ({ params }: PostEditPageProps) => {
+  const post = posts.find((post) => post.id === params.id);
+
   return (
     <>
       <BackButton text="Back To Posts" link="/posts/" />
